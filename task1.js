f5269c0730e421095bfaccd0e24e51b688ef2805
Muhamad Wildan Nursyamsi Abdullah Al-Harist <125271650+AbiPasundan@users.noreply.github.com>
2025-12-30 10:36:03 +0700

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,11 @@
+const array = [1, 4, 9, 16];
+
+// Pass a function to map
+const mapped = array.map((x) => x * 2);
+
+console.log(mapped);
+// Expected output: Array [2, 8, 18, 32]
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i] * 2)
+}
